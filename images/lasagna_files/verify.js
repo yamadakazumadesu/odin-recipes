128148efa0ad86a0ddff1c2bba9e2ae4637ed889
Neil <neilzapanta1@gmail.com>
2022-08-12 09:17:22 +0800

    index.html

---
+++ images/lasagna_files/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_806258485993({ImpressionID:"7787956770ac46a986b3a3c45078a339", ServerPublicDns:"rtbc-ae1.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "7787956770ac46a986b3a3c45078a339");}catch(e){}
+try{__verify_callback_806258485993({
+ResultID:2,
+Passback:"",
+AdWidth:300,
+AdHeight:600});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "7787956770ac46a986b3a3c45078a339");}catch(e){}
